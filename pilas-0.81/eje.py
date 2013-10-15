1a1a9d793c9409cdfc96b77bd699b33c33191fe8
apehua <agdelmonaco@linti.unlp.edu.ar>
2013-10-15 16:24:07 -0300

    Primera versión del simulador multiplo N6

---
+++ pilas-0.81/eje.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'eje.ui'
+#
+# Created: Tue Sep 10 13:27:06 2013
+#      by: PyQt4 UI code generator 4.9.3
+#
+# WARNING! All changes made in this file will be lost!
+
